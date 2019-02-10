f2e3d1650fa9704f3f52c2a9b7e6747796998df3
naveenezio <infantnaveen13@gmail.com>
2019-02-10 14:32:24 +0530

    fin

--- inctf mock crypto challenges/AES-1/decrypt.py
+++
@@ -1,7 +0,0 @@
-from Crypto.Cipher import AES
-
-key = "\xa2q\x8bu6Y\xa1\rT\xb7\xce\x86\x81S\xf2\xfc"
-flag = open("ciphertext.txt").read()
-obj1 = AES.new(key, AES.MODE_ECB)
-ct = obj1.decrypt((flag))
-open("flag.txt",'w').write(ct)
--- inctf mock crypto challenges/rsa/RSA-3/test.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Util.number import *
-from Crypto.PublicKey import RSA
-import gmpy2
-e=open('ciphertext.txt','r')
-
-e=bytes_to_long	(e.read())
-
-print(e)
