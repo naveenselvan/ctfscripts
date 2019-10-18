bfb1e27f317e3da4cd8b5cdb83000d7a7327e7cd
naveenezio <infantnaveen13@gmail.com>
2019-10-18 03:41:47 -0700

    asm

---
+++ asm/String_load_store.asm
@@ -0,0 +1,35 @@
+global main
+section .data
+source  db "PLaying With Strings",0
+len equ $-source
+
+section .bss
+destination resb 30
+
+
+section .text
+
+main:
+   mov ecx,len
+   mov esi,source
+   mov edi,destination
+   
+loo:
+   lodsb
+   stosb
+   loop loo
+
+
+   mov	edx,len        ;message length
+   mov	ecx,destination	        ;message to write
+   mov	ebx,1	        ;file descriptor (stdout)
+   mov	eax,4	        ;system call number (sys_write)
+   int	0x80	        ;call kernel
+
+   mov eax,1
+   int 0x80            
+
+   
+
+
+
