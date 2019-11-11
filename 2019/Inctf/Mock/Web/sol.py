3bb373733a5924961d62e6923dae268f17381999
naveenezio <infantnaveen13@gmail.com>
2019-11-11 12:07:51 +0530

    inc

--- 2019/Inctf/Mock/Web/sol.py
+++ 2019/Inctf/Mock/Web/sol.py
@@ -1,15 +1,15 @@
 import string
 import itertools
 
-key=[1847,1878,443,384]
+key=[1847,1878,443,384]               #runtime-dump
 
 
-source = "ТЄЂђЭБёщФКєѹЯћЅѹѺеЂЖХЌДГѸеДЙѴѕМ"
+source = "ТЄЂђЭБёщФКєѹЯћЅѹѺеЂЖХЌДГѸеДЙѴѕМ"        #static sring to be XOR-ed  
 
 crypt_code=''
 index=0
 for i in source:
-    crypt_code+=chr(ord(i)^(key[index%4]))
+    crypt_code+=chr(ord(i)^(key[index%4]))          #XOR-ed String
     index+=1
 
 
@@ -22,10 +22,10 @@ possibleset = string.ascii_letters + string.digits + "_@!?-{}"
 for hashind in range(4):
     print("-----------------")
     key=[]
-    for p in possibleset:     #Iterate through Strings
+    for p in possibleset:     #Iterate through Strings of 0,1,3,4
         testsol = ord(the_string[hashind]) ^ ord(p)  #Xor The string Index With P, First four letters are already tested
-        for j in range(hashind + 4, len(the_string), 4):     #Iterate Through Particular Index Of the The String
-            if chr(testsol ^ ord(the_string[j])) not in possibleset:  #If it not Present in the Printable Ascii Range Exit from the loop
+        for j in range(hashind + 4, len(the_string), 4):     #Iterate Through Particular Index Of the The String  
+            if chr(testsol ^ ord(the_string[j])) not in possibleset:  #If it not Present in the Printable Ascii Range Exit from the loop ensure it matches with the appropriate index
                 break
         else:
             print(testsol)
@@ -53,9 +53,10 @@ def crypt(enc, key):
 
 l1=[[892, 891, 810],[820, 821,828],[1498,1501,1500,1503],[1440, 1441, 1446]]
 for list in itertools.product(*l1):
-    index=index+1
+    #index=index+1
     temp=crypt(crypt_code, list)
     if 'inctf{' in temp:
+        print(temp)
         print(temp)
 
 
