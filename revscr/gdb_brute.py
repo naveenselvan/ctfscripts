b2f7f131f27a49e83a2381de38217584b11c4d7b
Naveen Selvan <infantnaveen13@gmail.com>
2019-11-19 18:49:49 +0530

    Create gdb_brute.py

---
+++ revscr/gdb_brute.py
@@ -0,0 +1,32 @@
+
+
+import gdb
+import string
+
+charset = string.ascii_lowercase
+key = ['a'] * 0xE
+
+def exp_write():
+    f = open('./exp', 'w')
+    f.write(''.join(key))
+    f.close()
+
+bp = gdb.Breakpoint('*0x0000000000400FF5')
+bp.silent = True
+
+for i in range(len(key)):
+    for c in charset:
+        key[i] = c
+        exp_write()
+        bp.ignore_count = i
+        gdb.execute('r < ./exp')
+        eflags = gdb.execute('p $eflags', to_string=True)
+        if 'ZF' in eflags:
+            break
+gdb.execute('c')
+gdb.execute('q')
+
+
+#https://github.com/j3rrry/Writeups/tree/master/CTF/2017/0x00/reversing/challenge-000
+
+#gdb -x script.py ./guessme
