3cde43e09753a035a763adfc1ef6bc4258d74cfc
naveenezio <infantnaveen13@gmail.com>
2020-02-12 13:35:05 +0530

    nullcon

---
+++ 2020/Nullcon/year3000.py
@@ -0,0 +1,39 @@
+import base64
+import sys
+import os
+from pathlib import Path
+
+def solve(filename):
+	file = open(filename, "rb").read()
+	ELF64_Addr = 0x1010
+	ELF32_Addr = 0x1008
+	
+	Format = ord(file[0x04])
+	if (Format == 2):
+                code_count=(ord(file[0x819]))
+                code_value=chr(ord(file[0x820]))
+		value = file[ELF64_Addr : ELF64_Addr+8]
+		
+             	flag = code_value * code_count + value
+
+	elif(Format == 1):
+
+                code_count=(ord(file[0x661]))
+                code_value=chr(ord(file[0x668]))
+		value = file[ELF32_Addr:ELF32_Addr+4]
+
+		flag = code_value * code_count + value
+	
+        print(flag)
+	#return flag
+
+
+
+
+def main():
+    solve(sys.argv[1])
+
+
+
+if __name__ == "__main__":
+	main()
